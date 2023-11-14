f12dac0989d5663d3acf4bc38f25c84891ae16ff
nurulinsn17 <150800757+nurulinsn17@users.noreply.github.com>
2023-11-14 18:49:10 +0700

    Add files via upload

---
+++ cv.php
@@ -0,0 +1,37 @@
+<html>
+<header>
+	<title>  </title>
+</header>
+<body>
+	<table border='0' width='100%' > 
+		<tr>
+			<td colspan='2' align='center' bgcolor='#556B2F' > <img src='header.jpg'> </td>
+		</tr>
+		<tr bgcolor='#D4CFB0'>
+			<td valign='top'align='center' width='25%'>
+				<table border='1' width='40%'>
+					<tr> 
+						<th> MENU </th>
+					</tr>
+					<tr> 
+						<td><a href='index.php' style="color:black;"> BERANDA </td>
+					</tr>
+					<tr> 
+						<td><a href='cv.php' style="color:black;"> CV </td>
+					</tr>
+					<tr>
+						<td><a href='galeri.php' style="color:black;"> GALERI </td>
+					</tr>
+				</table>
+			</td>
+			<td align='center' > <img src='cv.jpg' width='35%'> </td>
+		</tr>
+		<tr>
+			<td colspan='2' bgcolor='#556B2F'align='center' height='25%' > 
+				<footer> <p style="color:#D4CFB0;">© 2023 Nurul Insan</p> </footer> 
+			</td>
+		</tr>
+		
+	</table>
+</body>
+</html>
---
+++ galeri.php
@@ -0,0 +1,48 @@
+<html>
+<header>
+	<title> </title>
+</header>
+<body>
+	<table border='0' width='100%' > 
+		<tr>
+			<td colspan='2' align='center' bgcolor='#556B2F' > <img src='header.jpg'> </td>
+		</tr>
+		<tr bgcolor='#D4CFB0'>
+			<td valign='top' align='center' width='25%'>
+				<table border='1' width='40%'>
+					<tr> 
+						<th> MENU </th>
+					</tr>
+					<tr> 
+						<td><a href='index.php' style="color:black;"> BERANDA </td>
+					</tr>
+					<tr> 
+						<td><a href='cv.php' style="color:black;"> CV </td>
+					</tr>
+					<tr>
+						<td><a href='galeri.php' style="color:black;"> GALERI </td>
+					</tr>
+				</table>
+			</td>
+			<td align='center'>
+				<table border='0' width='80%' align='left'>
+					<tr>
+						<td><img src='off.jpeg' width='100%'></td>
+						<td><img src='force.jpeg' width='100%'></td>
+						<td><img src='view.jpeg' width='100%'></td>
+						<td><img src='love.jpeg' width='100%'></td>
+						
+					</tr>
+					
+				</table>	 
+			</td>
+		</tr>
+		<tr>
+			<td colspan='2' bgcolor='#556B2F'align='center' height='25%' > 
+				<footer> <p style="color:#D4CFB0;">© 2023 Nurul Insan</p> </footer> 
+			</td>
+		</tr>
+		
+	</table>
+</body>
+</html>
